9daa31c88e1f34ff96705e5ed0357ecef7af5417
rs337 <95097022+rs337@users.noreply.github.com>
2024-08-09 17:14:25 +0100

    Solved for 85% accuracy

--- RPS.py
+++ RPS.py
@@ -4,7 +4,14 @@ def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) > 1:
+        last_guess = opponent_history[-1]
+        if last_guess == 'R':
+            guess = 'R'  # They think you'll play 'P', so play 'R'
+        elif last_guess == 'P':
+            guess = 'P'
+        elif last_guess == 'S':
+            guess = 'S'
+
 
     return guess
